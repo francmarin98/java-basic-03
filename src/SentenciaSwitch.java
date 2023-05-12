67afa6506d4a21ea209314087cd67fcb6fb2ebbd
francmarin98 <francmarinc@gmail.com>
2023-05-11 22:42:28 -0500

    push

--- src/SentenciaSwitch.java
+++ src/SentenciaSwitch.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class SentenciaSwitch {
+import java.util.Scanner;
+
+public class SentenciaSwitch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        switch (num){
+            case 0:
+                System.out.println("Es cero");
+                break;
+
+            case 1:
+                System.out.println("Es uno");
+                break;
+
+            case 2:
+                System.out.println("Es dos");
+                break;
+
+            case 3:
+                System.out.println("Es tres");
+                break;
+
+            default:
+                System.out.println("No se que número es jeje");
+                break;
+        }
+
+
+    }
 }
